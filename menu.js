532bea08212430a9d63e5ab570f18803179d3bc8
Nz0M <enzo.martin78@gmail.com>
2025-10-29 20:36:31 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector('.burger');
+  const navLinks = document.getElementById('nav-links');
+
+  if (burger && navLinks) {
+    burger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+      burger.classList.toggle('toggle');
+    });
+  }
+});
